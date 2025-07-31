131024702a612e197ef2e8cdbdc314e5cdf9c853
Samuix <sami.khalil.pro@hotmail.com>
2025-07-31 19:08:49 +0200

    Improve mobile sliding left and right

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'; // <-- THIS should import your App component
 import './App.css';
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
